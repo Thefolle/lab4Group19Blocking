721172d864747fad22f3b698b24c47728346ff9d
Davide Calarco <s280174@studenti.polito.it>
2021-05-04 16:31:49 +0200

    Initial commit

---
+++ src/main/kotlin/it/polito/group19/lab4blocking/Lab4blockingApplication.kt
@@ -0,0 +1,11 @@
+package it.polito.group19.lab4blocking
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Lab4blockingApplication
+
+fun main(args: Array<String>) {
+    runApplication<Lab4blockingApplication>(*args)
+}
---
+++ src/test/kotlin/it/polito/group19/lab4blocking/Lab4blockingApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.polito.group19.lab4blocking
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Lab4blockingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
